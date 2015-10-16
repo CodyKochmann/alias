b5500cb06a29076fad02bfe02a276ca06aea48d7
Cody L. Kochmann <kochmanncody@gmail.com>
2015-10-16 11:15:17 -0700

    solidified the aliases I have and posted them in the initial bash aliases file.

---
+++ installer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2015-10-16 10:06:16
+# @Last Modified by:   codykochmann
+# @Last Modified time: 2015-10-16 11:14:23
