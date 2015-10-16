919f721840d75e9974401469011e4482fac59bf3
Cody L. Kochmann <kochmanncody@gmail.com>
2015-10-16 11:30:08 -0700

    wrote up the installer script

--- installer.py
+++ installer.py
@@ -3,4 +3,36 @@
 # @Author: Cody Kochmann
 # @Date:   2015-10-16 10:06:16
 # @Last Modified by:   codykochmann
-# @Last Modified time: 2015-10-16 11:14:23
+# @Last Modified time: 2015-10-16 11:29:39
+
+bashrc_file="~/.bashrc"
+bash_aliases_file="~/.bash_aliases"
+
+def append_to_file(filename,input_string):
+  with open(filename, 'a') as f:
+    f.write(input_string)
+
+def grep(url):
+  import os
+  return(str(os.popen("wget -qO- %s" % (url)).read()))
+
+def bash(text_to_run):
+  import os
+  os.system(text_to_run)
+
+remote_bash_aliases_text=grep("https://raw.githubusercontent.com/CodyKochmann/alias/master/bash_aliases")
+
+append_to_file(bash_aliases_file, remote_bash_aliases_text)
+
+to_append_to_bashrc="""
+# automatically loads the bash aliases from ~/.bash_aliases
+bash %s
+""" % (bash_aliases_file)
+
+append_to_file(bashrc_file, to_append_to_bashrc)
+
+bash("bash %s"%(bash_aliases_file))
+bash("alias -p")
+print("""
+      All aliases have been loaded.
+      """)
