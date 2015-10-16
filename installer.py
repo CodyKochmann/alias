9206c299e1290bdc796a9551acd07a2cd3070146
Cody L. Kochmann <kochmanncody@gmail.com>
2015-10-16 11:53:33 -0700

    minified the link used to get the aliases

--- installer.py
+++ installer.py
@@ -3,10 +3,12 @@
 # @Author: Cody Kochmann
 # @Date:   2015-10-16 10:06:16
 # @Last Modified by:   codykochmann
-# @Last Modified time: 2015-10-16 11:30:41
+# @Last Modified time: 2015-10-16 11:53:14
 
 bashrc_file="~/.bashrc"
 bash_aliases_file="~/.bash_aliases"
+
+remote_alias_link="http://bit.ly/codys-aliases"
 
 def append_to_file(filename,input_string):
   with open(filename, 'a') as f:
@@ -20,7 +22,7 @@ def bash(text_to_run):
   import os
   os.system(text_to_run)
 
-remote_bash_aliases_text=grep("https://raw.githubusercontent.com/CodyKochmann/alias/master/bash_aliases")
+remote_bash_aliases_text=grep(remote_alias_link)
 
 append_to_file(bash_aliases_file, remote_bash_aliases_text)
 
